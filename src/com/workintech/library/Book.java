66646839e9ca1cb6487d7e68a77d0f48d55c8766
Berra Karaman <esma.berra.karaman@hotmail.com>
2025-03-24 21:54:57 +0300

    initialize library

---
+++ src/com/workintech/library/Book.java
@@ -0,0 +1,4 @@
+package com.workintech.library;
+
+public class Book {
+}
---
+++ src/com/workintech/library/Library.java
@@ -0,0 +1,10 @@
+package com.workintech.library;
+
+import java.util.Map;
+
+public class Library {
+    private Map<Books> books;
+    private Map<Reader> readers;
+
+
+}
