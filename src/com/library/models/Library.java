5a77c85f7813fbc2b17066af8d259b4b3bf92903
Berra Karaman <esma.berra.karaman@hotmail.com>
2025-04-01 10:58:49 +0300

    create library service

---
+++ src/com/library/models/Library.java
@@ -0,0 +1,28 @@
+package com.library.models;
+
+import java.util.Map;
+
+public class Library {
+    private Map<Long, Book> books;
+    private Map<Long, Reader> readers;
+
+    public Library(Map<Long, Book> books) {
+        this.books = books;
+    }
+
+    public Map<Long, Book> getBooks() {
+        return books;
+    }
+
+    public Map<Long, Reader> getReaders() {
+        return readers;
+    }
+
+    public void setBooks(Map<Long, Book> books) {
+        this.books = books;
+    }
+
+    public void setReaders(Map<Long, Reader> readers) {
+        this.readers = readers;
+    }
+}
---
+++ src/com/library/service/LibraryService.java
@@ -0,0 +1,33 @@
+package com.library.service;
+
+import com.library.models.Library;
+import com.library.models.Reader;
+
+public class LibraryService {
+    private Library library;
+
+    public LibraryService(Library library) {
+        this.library = library;
+    }
+
+    public void getReader() {
+
+    }
+
+    public void newBook() {
+
+    }
+
+    public void lendBook() {
+
+    }
+
+    public void takeBackBook() {
+
+    }
+
+    public void showBook() {
+
+    }
+}
+
