a82545fdefc31fd979c0e99ff711fecb34877da9
Berra Karaman <esma.berra.karaman@hotmail.com>
2025-04-06 23:58:39 +0300

    add introduction and role flow to app

--- src/com/library/console/App.java
+++ src/com/library/console/App.java
@@ -1,7 +1,22 @@
 package com.library.console;
 
+import com.library.utils.Input;
+
 public class App {
+    private final Input input = new Input();
+
     public void run() {
-        System.out.println("App is running.");
+        System.out.println("\nWelcome, this is the library management system.\n");
+        askUserRole();
+    }
+
+    public void askUserRole() {
+        System.out.println("Who are you?");
+        System.out.println("1. Reader");
+        System.out.println("2. Author");
+        System.out.println("3. Librarian");
+
+        int selection = input.readIntRange("", 1, 3);
+        System.out.println("Your selection => " + selection);
     }
 }
