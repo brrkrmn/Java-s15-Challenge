67d8ca2576c5e114cb7b23ec36be6ea55abecd99
Berra Karaman <esma.berra.karaman@hotmail.com>
2025-04-06 23:57:52 +0300

    create scanner util class

---
+++ src/com/library/utils/Input.java
@@ -0,0 +1,34 @@
+package com.library.utils;
+
+import java.util.Scanner;
+
+public class Input {
+    private final Scanner scanner = new Scanner(System.in);
+
+    public String readLine(String prompt) {
+        System.out.println(prompt);
+        return scanner.nextLine();
+    }
+
+    public int readInt(String prompt) {
+        while (true) {
+            System.out.println(prompt);
+            String userInput = scanner.nextLine();
+            try {
+                return Integer.parseInt(userInput);
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter a number.");
+            }
+        }
+    }
+
+    public int readIntRange(String prompt, int min, int max) {
+        while (true) {
+            int value = readInt(prompt);
+            if (value >= min && value <= max) {
+                return value;
+            }
+            System.out.printf("Please enter a number between %d and %d.%n", min, max);
+        }
+    }
+}
